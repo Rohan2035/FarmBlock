be9532a57c95a4af0ad691af7ab6236cecf76496
Rohan2035 <rohan12gupta9@gmail.com>
2022-04-26 17:55:54 +0530

    Final Fixes ⚒⚒

--- Blog/Post/urls.py
+++ Blog/Post/urls.py
@@ -2,19 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostFunc.index, name="Index"),
-    path('content/<str:slug>', views.PostFunc.content, name="content"),
-    path('category/<str:Category>', views.PostFunc.category, name="category"),
-    path('search/', views.PostFunc.search, name="search"),
-    path('addContent/', views.PostFunc.addContent, name="addContent"),
-    path('contact/', views.PostFunc.contact, name="contact"),
-    path('vc/', views.PostFunc.validateContact, name="validateContact"),
-    path('validate/', views.PostFunc.validate, name="validate"),
-    path('signup/', views.utility.handleSignup, name="signup"),
-    path('login/', views.utility.handleLogin, name="login"),
-    path('logout/', views.utility.handleLogout, name="logout"),
-    path('displayall/', views.PostFunc.DisplayAll, name="display"),
-    path('userposts/', views.userUtility.userPost, name="user_post"),
-    # Remove this
-    path('test/', views.PostFunc.test, name="user_post"),
+    path('', views.index, name="Index"),
+    path('content/<str:slug>', views.content, name="content"),
+    path('category/<str:Category>', views.category, name="category"),
+    path('search/', views.search, name="search"),
+    path('addContent/', views.addContent, name="addContent"),
+    path('contact/', views.contact, name="contact"),
+    path('vc/', views.validateContact, name="validateContact"),
+    path('validate/', views.validate, name="validate"),
+    path('signup/', views.handleSignup, name="signup"),
+    path('login/', views.handleLogin, name="login"),
+    path('logout/', views.handleLogout, name="logout"),
+    path('displayall/', views.DisplayAll, name="display"),
+    path('userposts/', views.userPost, name="user_post"),
 ]
--- Blog/Post/views.py
+++ Blog/Post/views.py
@@ -8,121 +8,121 @@ import random as rn
 
 
 # Class that handles posts
-class PostFunc():
-
-    def index(request):
-        a = []
-
-        for i in range(1, 7):
-            a.append(bp.objects.get(post_id = i))
-
-        all_post = list(bp.objects.all())
-        items = rn.sample(all_post, 6)
-
-        context = {'post' : items}
-
-        return render(request, 'index.html', context)
 
 
-    def content(request, slug):
+def index(request):
+    a = []
 
-        # Fetching the Post
-        post = bp.objects.get(slug = slug)
+    for i in range(1, 7):
+        a.append(bp.objects.get(post_id = i))
 
-        # Suggestions
-        all_post = list(bp.objects.all())
-        items = rn.sample(all_post, 3)
+    all_post = list(bp.objects.all())
+    items = rn.sample(all_post, 6)
 
-        context = {'post' : post, 'post1' : items}
-        return render(request, 'content.html', context)
+    context = {'post' : items}
 
-
-    def category(request, Category):
-        post = bp.objects.filter(Category=Category)
-        context = {"cat" : post}
-        return render(request, 'cat.html', context)
-    
-
-    def search(request):
-
-        query = request.GET['query']
-        print(query)
-
-        query = " ".join(query.split())
-
-        if(len(query) == 0):
-            return HttpResponse("Search unsucessful please fill the search box")
-
-        elif(len(query) > 1000):
-            return HttpResponse("You have filled a long character please enter a smaller keyword")
-
-        else:
-            post = bp.objects.filter(Name__icontains=query)
-            context = {'post': post}
-
-        return render(request, 'search.html', context)
+    return render(request, 'index.html', context)
 
 
-    def addContent(request):
+def content(request, slug):
 
-        if request.user.is_authenticated:
-            return render(request, 'addContent.html')
+    # Fetching the Post
+    post = bp.objects.get(slug = slug)
 
-        else:
-            return HttpResponse("Error 404 Not found")
+    # Suggestions
+    all_post = list(bp.objects.all())
+    items = rn.sample(all_post, 3)
 
-
-    def validate(request):
-
-        if request.method == 'POST':
-
-            name = request.POST['name']
-            category = request.POST['cat']
-            post = request.POST['post']
-            d = request.POST['date']
-            
-            img = request.FILES["photo"]
+    context = {'post' : post, 'post1' : items}
+    return render(request, 'content.html', context)
 
 
-            # Few basic checks
-            slug = str(name).lower()
-            slug = "".join(slug.split())
-
-            name = name.lower()
-
-            category = category.lower()
-            category = "".join(category.split())
-
-            a = request.user
-            a = str(a)
-
-            # Use this checks while deployment
-            if(len(name) > 3 and len(category) > 3 and len(post) > 50):
-                pass
+def category(request, Category):
+    post = bp.objects.filter(Category=Category)
+    context = {"cat" : post}
+    return render(request, 'cat.html', context)
 
 
-            final = bp(Name=name, Category=category, content=post, slug=slug, img1=img, date=d, author = a)
-            
-            final.save()  
-            messages.success(request, "Post Sucessfully added") 
-            return redirect("Index")
+def search(request):
 
-        else:
+    query = request.GET['query']
+    print(query)
 
-            return HttpResponse("Error 404 not found")
+    query = " ".join(query.split())
+
+    if(len(query) == 0):
+        return HttpResponse("Search unsucessful please fill the search box")
+
+    elif(len(query) > 1000):
+        return HttpResponse("You have filled a long character please enter a smaller keyword")
+
+    else:
+        post = bp.objects.filter(Name__icontains=query)
+        context = {'post': post}
+
+    return render(request, 'search.html', context)
 
 
-    def DisplayAll(request):
-        post = bp.objects.all()
-        context = {"post" : post}
-        return render(request, "DisplayAll.html", context)
+def addContent(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'addContent.html')
+
+    else:
+        return HttpResponse("Error 404 Not found")
 
 
-    def contact(request):
-        return render(request, "Contact.html")
+def validate(request):
+
+    if request.method == 'POST':
+
+        name = request.POST['name']
+        category = request.POST['cat']
+        post = request.POST['post']
+        d = request.POST['date']
+        
+        img = request.FILES["photo"]
 
 
-    def validateContact(request):
+        # Few basic checks
+        slug = str(name).lower()
+        slug = "".join(slug.split())
+
+        name = name.lower()
+
+        category = category.lower()
+        category = "".join(category.split())
+
+        a = request.user
+        a = str(a)
+
+        # Use this checks while deployment
+        if(len(name) > 3 and len(category) > 3 and len(post) > 50):
+            pass
+
+
+        final = bp(Name=name, Category=category, content=post, slug=slug, img1=img, date=d, author = a)
+        
+        final.save()  
+        messages.success(request, "Post Sucessfully added") 
+        return redirect("Index")
+
+    else:
+
+        return HttpResponse("Error 404 not found")
+
+
+def DisplayAll(request):
+    post = bp.objects.all()
+    context = {"post" : post}
+    return render(request, "DisplayAll.html", context)
+
+
+def contact(request):
+    return render(request, "Contact.html")
+
+
+def validateContact(request):
 
         if request.method == 'POST':
             name = request.POST['name']
@@ -140,78 +140,64 @@ class PostFunc():
         else:
             return HttpResponse("Error not found")
 
-
-
-    # Remove This
-    def test(request):
-        posts = list(bp.objects.all())
-        items = rn.sample(posts, 6)
-
-        print(items[3].author)
-
-        return HttpResponse("Test")        
+      
 
 
 
 
 # Class that handles user login
-class utility():
 
-    def handleSignup(request):
+
+def handleSignup(request):
+    
+    if request.method == 'POST':
+
+        name = request.POST['name']
+        mail = request.POST['email']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+
+        #Basic Checks
+        check = User.objects.filter(email = mail).exists() 
+        if (check):
+            messages.warning(request, 'account already exists')
+
+        elif (password != confirmPassword):
+            messages.warning(request, "Passwords don't match")
+
+        elif (len(password) < 5):
+            messages.warning(request, 'Your passsword is too short')
+
+        else:       
+            #Create User
+            myuser = User.objects.create_user(name, mail, password)
+            myuser.save()
+            messages.success(request, f'Welcome to FarmBlock {name}') 
+
         
-        if request.method == 'POST':
+        return redirect('Index')
 
-            name = request.POST['name']
-            mail = request.POST['email']
-            password = request.POST['password']
-            confirmPassword = request.POST['confirmPassword']
-
-            #Basic Checks
-            check = User.objects.filter(email = mail).exists() 
-            if (check):
-                messages.warning(request, 'account already exists')
-
-            elif (password != confirmPassword):
-                messages.warning(request, "Passwords don't match")
-
-            elif (len(password) < 5):
-                messages.warning(request, 'Your passsword is too short')
-
-            else:       
-                #Create User
-                myuser = User.objects.create_user(name, mail, password)
-                myuser.save()
-                messages.success(request, f'Welcome to FarmBlock {name}') 
-
-            
-            return redirect('Index')
-
-        else:
-            return HttpResponse("404 Error no resource found")
+    else:
+        return HttpResponse("404 Error no resource found")
 
 
-    def handleLogin(request):
+def handleLogin(request):
+    
+    if request.method == 'POST':
         
-        if request.method == 'POST':
-            
-            mail = request.POST['loginEmail']
-            passw = request.POST['loginPassword']
-            check = User.objects.filter(email = mail).exists()
+        mail = request.POST['loginEmail']
+        passw = request.POST['loginPassword']
+        check = User.objects.filter(email = mail).exists()
 
-            if(check):
+        if(check):
 
-                name = User.objects.get(email=mail).username
-                us = authenticate(username = name, password = passw)
+            name = User.objects.get(email=mail).username
+            us = authenticate(username = name, password = passw)
 
-                if us is not None:
-                    login(request, us)
-                    messages.success(request, "Login Sucessful")
-                    return redirect("Index")
-
-                else:
-                    messages.warning(request, "Please enter valid credentials")
-                    return redirect ("Index")
-
+            if us is not None:
+                login(request, us)
+                messages.success(request, "Login Sucessful")
+                return redirect("Index")
 
             else:
                 messages.warning(request, "Please enter valid credentials")
@@ -219,28 +205,32 @@ class utility():
 
 
         else:
-            return HttpResponse("Error 404")
-                
+            messages.warning(request, "Please enter valid credentials")
+            return redirect ("Index")
 
-    def handleLogout(request):
+
+    else:
+        return HttpResponse("Error 404")
+            
+
+def handleLogout(request):
         logout(request)
         messages.success(request, "Sucessfully logged out")
         return redirect("Index")
 
 
 # Class to see all the posts entered by the user
-class userUtility():
 
-    def userPost(request):
+def userPost(request):
+    
+    name = request.user
+
+    if (bp.objects.filter(author = name).exists()):
+        post = bp.objects.filter(author = name)
         
-        name = request.user
+        context = {'post' : post}
+        return render(request, 'userposts.html', context)
 
-        if (bp.objects.filter(author = name).exists()):
-            post = bp.objects.filter(author = name)
-            
-            context = {'post' : post}
-            return render(request, 'userposts.html', context)
-
-        else:
-            return HttpResponse("<h1 align='center'>No posted added</h1>")
+    else:
+        return HttpResponse("<h1 align='center'>No posted added</h1>")
 
